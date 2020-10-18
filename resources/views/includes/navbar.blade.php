851391d4459f79a26896c9406b14fbd193ac2e41
Magnus Andersson <bredsjomagnus@yahoo.se>
2020-10-18 11:46:47 +0200

    deleted link to create new user

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -36,11 +36,11 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
             </li>
-            <li class="nav-item">
+            <!-- <li class="nav-item">
               <a class="dropdown-item" href="{{ url('user/create') }}">
                         Registrera ny användare
               </a>
-            </li>
+            </li> -->
             
         @else
             <li class="nav-item dropdown">
